d4c4fb829f43271abfb885f55ff2cb5615ba0c34
stiffjobs <charleschen0306@gmail.com>
2023-09-17 21:31:29 +0800

    update for gracefully shutdown

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -17,6 +19,9 @@ func (app *application) serve() error {
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
+	//Create a shutdownError channel. We will use this to receive any errors returned
+	//by the graceful Shutdown() function.
+	shutdownError := make(chan error)
 	go func() {
 		quit := make(chan os.Signal, 1)
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
@@ -25,13 +30,42 @@ func (app *application) serve() error {
 		app.logger.PrintInfo("caught signal", map[string]string{
 			"signal": s.String(),
 		})
+		//Create a context with a 30-second timeout.
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 
-		os.Exit(0)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
 	}()
+
 	app.logger.PrintInfo("starting server", map[string]string{
 		"addr": srv.Addr,
 		"env":  app.config.env,
 	})
 
-	return srv.ListenAndServe()
+	//calling shutdown() on our server will cause listenandserve() to immediately
+	//return a http.errserverclosed error. So if we see this error, it is actually a
+	//good thing and an indication that the graceful shutdown has started. So we check
+	//specially for this, only returning the error if it is not http.errserverclosed.
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	//Otherwise, we wait to receive the return value from Shutdown() on the
+	//shutdownError channel. If return value is an error,we know that there was a
+	//problem with the graceful shutdown and we return the error
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	//At this point we know that the graceful shutdown completed successfully and we
+	//log a "shopped server" message.
+	app.logger.PrintInfo("stopped server", map[string]string{
+		"addr": srv.Addr,
+	})
+
+	return nil
+
 }
